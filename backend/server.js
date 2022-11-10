a1a1a8103702930057ef585297ff44d9c10d19be
Asif saheer <asifsaheer70334@gmail.com>
2022-11-11 00:08:12 +0530

    cores updaedd

--- backend/server.js
+++ backend/server.js
@@ -17,20 +17,9 @@ const PORT = process.env.PORT || 9000;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
+app.use(cors({origin:"*"}))
 app.use(session({ secret: "key", cookie: { maxAge: 6000000 } }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept"
-  );
-  // res.setHeader(
-  //   "Access-Control-Allow-Methods",
-  //   "GET,POST,PUT,PATCH,DELETE,OPTIONS"
-  // );
-  next();
-});
+
 // user routes
 app.use("/api/user",userRoutes);
 app.use("/api/admin", adminRoutes);
