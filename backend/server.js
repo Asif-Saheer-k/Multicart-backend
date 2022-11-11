331cff62ac6533fc9a24ef1a1fa6ec9ed8206d67
Asif saheer <asifsaheer70334@gmail.com>
2022-11-11 23:51:06 +0530

    adkfjdf

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const db = require("./config/db");
-const cors=require('cors')
+const cors = require("cors");
 const path = require("path");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const userRoutes = require("./userRoutes/authuserRoutes");
 const adminRoutes = require("./adminRoutes/adminRoutes");
-const cartRoutes=require("./userRoutes/cartRoutes")
-const morgan=require('morgan')
+const cartRoutes = require("./userRoutes/cartRoutes");
+const morgan = require("morgan");
 __dirname = path.resolve();
 require("dotenv").config({ path: __dirname + "/.env" });
 
@@ -15,26 +15,39 @@ const PORT = process.env.PORT || 9000;
 
 const app = express();
 const oneDay = 1000 * 60 * 60 * 24;
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({origin:"*",methods:['GET','POST']}))
 app.use(cookieParser());
 
-app.use(session({
-  secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-  saveUninitialized:true,
-  cookie: { maxAge: oneDay },
-  resave: false 
-}));
-app.use(morgan('combined'));
-  
+app.use(
+  session({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+  })
+);
+app.use(morgan("combined"));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+  next();
+});
+
 // user routes
-app.use("/api/user",userRoutes);
+app.use("/api/user", userRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/api/user/cart",cartRoutes)
- 
+app.use("/api/user/cart", cartRoutes);
+
 //database connection
-db.connect((err) => {    
+db.connect((err) => {
   if (err) {
     console.log("connection error" + err);
   } else {
